4d4721096d456aa1288c0c810eba5e2f1a40e134
David Pochet <david.pochet@gmail.com>
2023-01-02 18:18:06 +0100

    Badges generation implementation

--- src/tests/test_badges.py
+++ src/tests/test_badges.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 from nmk.tests.tester import NmkBaseTester
@@ -8,13 +9,38 @@ class TestProtoPlugin(NmkBaseTester):
     def templates_root(self) -> Path:
         return Path(__file__).parent / "templates"
 
-    def prepare_proto_project(self, other_plugin: str = None) -> Path:
-        # Build a sample project with proto files
-        return self.prepare_project(f"ref_badges{('_'+other_plugin) if other_plugin is not None else ''}.yml")
+    @property
+    def readme(self) -> Path:
+        return self.test_folder / "README.md"
 
-    def escape(self, to_escape: Path) -> str:
-        # Escape backslashes (for Windows paths in json print)
-        return str(to_escape).replace("\\", "\\\\")
+    def prepare_badge_project(self, readme: str = None) -> Path:
+        # Build a sample project with proto files
+        if readme is not None:
+            shutil.copyfile(self.template(f"{readme}.md"), self.readme)
+        return self.prepare_project("ref_badges.yml")
+
+    def compare_readme(self, readme: str, is_ok: bool = True):
+        with self.template(f"{readme}.md").open() as a, self.readme.open() as b:
+            if is_ok:
+                assert a.readlines() == b.readlines()
+            else:
+                assert a.readlines() != b.readlines()
 
     def test_version(self):
-        self.nmk(self.prepare_proto_project(), extra_args=["version"])
+        self.nmk(self.prepare_badge_project(), extra_args=["version"])
+
+    def test_missing_begin_pattern(self):
+        self.nmk(self.prepare_badge_project("no_begin"), extra_args=["badges"])
+        self.check_logs("WARNING ❗ - Invalid or missing pattern in README.md file")
+        self.compare_readme("no_begin")
+
+    def test_missing_end_pattern(self):
+        self.nmk(self.prepare_badge_project("no_end"), extra_args=["badges"])
+        self.check_logs("WARNING ❗ - Invalid or missing pattern in README.md file")
+        self.compare_readme("no_end")
+
+    def test_ok(self):
+        self.nmk(self.prepare_badge_project("ok"), extra_args=["badges"])
+        self.compare_readme("no_end", False)
+        with self.readme.open() as r:
+            assert "[![Some text](http://foo.org/bar.png)](http://path.to/foo)" in r.read()

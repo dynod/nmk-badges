a72047ba39f609a39140bf17f0402ba0fc032395
David Pochet <david.pochet@gmail.com>
2024-12-29 18:18:45 +0100

    Updates from other plugins upgrade
    + change license for V1

--- src/nmk_badges/__init__.py
+++ src/nmk_badges/__init__.py
@@ -1,21 +1,13 @@
-from configparser import ConfigParser
-from pathlib import Path
+from importlib.metadata import version
 
 from nmk_base.version import VersionResolver
-from pkg_resources import DistributionNotFound, get_distribution
 
 __title__ = "nmk-badges"
 try:
-    __version__ = get_distribution(__title__).version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = version(__title__)
+except Exception:  # pragma: no cover
     # For debug
-    try:
-        with (Path(__file__).parent.parent.parent / "setup.cfg").open("r") as f:
-            c = ConfigParser()
-            c.read_file(f.readlines())
-            __version__ = c.get("metadata", "version")
-    except Exception:
-        __version__ = "unknown"
+    __version__ = "unknown"
 
 
 class NmkBadgesVersionResolver(VersionResolver):
